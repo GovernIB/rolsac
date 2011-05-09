bd40b74fc70cf5cdab502382792af05c41eb015b
ejaen-dgtic <ejaen-dgtic@58ef2255-6f3f-491f-b93b-e266552ec6fa>
2011-05-09 14:33:01 +0000

    integració canvis DGTIC (materia sense classificar, urls en sar, tests, refactoritzacio Actualitzador, refactoritzacio algunes actions del backoffice)
    git-svn-id: https://svn.code.sf.net/p/rolsac/code/rolsac-core/trunk@7 58ef2255-6f3f-491f-b93b-e266552ec6fa

---
+++ test/back/unitario/java/test/unitario/back/mock/MockIniciacionDelegate.java
@@ -0,0 +1,22 @@
+package test.unitario.back.mock;
+
+import java.rmi.RemoteException;
+
+import javax.ejb.EJBException;
+
+import org.ibit.rol.sac.persistence.delegate.IniciacionDelegate;
+import org.ibit.rol.sac.persistence.ejb.IniciacionFacadeEJB;
+
+public class MockIniciacionDelegate extends IniciacionDelegate {
+
+	public void ejbActivate() throws EJBException, RemoteException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void ejbPassivate() throws EJBException, RemoteException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
