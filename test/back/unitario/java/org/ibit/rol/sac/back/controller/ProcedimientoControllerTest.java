cfdaafc1523dd0a1562b115289f1808c4c238b3a
ejaen-dgtic <ejaen-dgtic@58ef2255-6f3f-491f-b93b-e266552ec6fa>
2011-10-06 07:55:52 +0000

    mid: 0000043 msg: (refactorització) substitució caràcters latins per ascii
    git-svn-id: https://svn.code.sf.net/p/rolsac/code/rolsac-core/branches/rolsac-1.1@151 58ef2255-6f3f-491f-b93b-e266552ec6fa

--- test/back/unitario/java/org/ibit/rol/sac/back/controller/ProcedimientoControllerTest.java
+++ test/back/unitario/java/org/ibit/rol/sac/back/controller/ProcedimientoControllerTest.java
@@ -43,12 +43,12 @@ public class ProcedimientoControllerTest extends TestCase {
 	  MockMateriaDelegate materiaDelegate = new MockMateriaDelegate() { 
 		  public List listarMaterias() throws org.ibit.rol.sac.persistence.delegate.DelegateException {
 			  List<Materia> materias=new ArrayList<Materia>();
-			  materias=añadirMateria(materias,Materia.CE_SENSECLASSIFICAR);
-			  materias=añadirMateria(materias,"Salut");
+			  materias=anadirMateria(materias,Materia.CE_SENSECLASSIFICAR);
+			  materias=anadirMateria(materias,"Salut");
 			  return materias;
 		  }
 
-		  private List<Materia> añadirMateria(List<Materia> materias, String cemateria) {
+		  private List<Materia> anadirMateria(List<Materia> materias, String cemateria) {
 			  Materia materia;
 			  materia = new Materia();
 			  materia.setCodigoEstandar(cemateria);
