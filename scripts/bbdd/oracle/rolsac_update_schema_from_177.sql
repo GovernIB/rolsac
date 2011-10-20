a2fdf7dca25a3b26bbd244e8897e70d09d882241
mgonzalez-ibit <mgonzalez-ibit@58ef2255-6f3f-491f-b93b-e266552ec6fa>
2011-10-20 08:42:00 +0000

    mid:0000072 msg:Error als scripts d'update de la versió 177.
    git-svn-id: https://svn.code.sf.net/p/rolsac/code/rolsac-core/branches/rolsac-1.0@200 58ef2255-6f3f-491f-b93b-e266552ec6fa

--- scripts/bbdd/oracle/rolsac_update_schema_from_177.sql
+++ scripts/bbdd/oracle/rolsac_update_schema_from_177.sql
@@ -15,6 +15,14 @@ ADD
 ALTER TABLE RSC_NORMAT ADD CONSTRAINT RSC_NERADM_FK FOREIGN KEY (NER_CODADM) REFERENCES RSC_ADMREM;
 
 
+alter table RSC_DOCUME drop constraint RSC_DOCARC_FK;
+
+alter table RSC_DOCUME add column DOC_CODARC2 NUMBER(19,0);
+update RSC_DOCUME set DOC_CODARC2 = DOC_CODARC;
+alter table RSC_DOCUME drop column DOC_CODARC;
+
+alter table RSC_DOCUME add constraint RSC_DOCARC2_FK foreign key (DOC_CODARC2) references RSC_ARCHIV;
+
 ALTER TABLE RSC_DOCUME
 ADD
    (
@@ -22,8 +30,19 @@ ADD
 	  DOR_URLREM varchar2(512), 
 	  DOR_CODADM number(19,0) 
    );
+   
+
 
 ALTER TABLE RSC_DOCUME ADD CONSTRAINT RSC_DORADM_FK FOREIGN KEY (DOR_CODADM) REFERENCES RSC_ADMREM;
 
 
 alter table RSC_TRAMIT add TRA_DATACTUVUDS varchar2(255);
+
+alter table RSC_TAXA drop constraint RSC_CODITRA_FK;
+
+
+alter table RSC_TAXA add column TAX_CODTRA NUMBER(19,0);
+update RSC_TAXA set TAX_CODTRA = CODITRA;
+alter table RSC_TAXA drop column CODITRA;
+alter table RSC_TAXA add constraint RSC_TAXTRA_FK foreign key (TAX_CODTRA) references RSC_TRAMIT;
+
