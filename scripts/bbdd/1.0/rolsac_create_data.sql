b62e1a1e120c157c3489dba3e75c9efa59d33459
anadal-ibit <anadal-ibit@58ef2255-6f3f-491f-b93b-e266552ec6fa>
2012-03-12 13:38:17 +0000

    mid: 0000262 msg: Reestructurar scripts de bbdd segons el document de qualitat en la forja
    git-svn-id: https://svn.code.sf.net/p/rolsac/code/rolsac-core/branches/rolsac-1.0@437 58ef2255-6f3f-491f-b93b-e266552ec6fa

---
+++ scripts/bbdd/1.0/rolsac_create_data.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO rsc_idioma(idi_codi, idi_orden, idi_codest, idi_nombre) VALUES('ca', 1, 'ca', 'Català');
+INSERT INTO rsc_idioma(idi_codi, idi_orden, idi_codest, idi_nombre) VALUES('es', 2, 'es', 'Castellano');
+INSERT INTO rsc_usuari(usu_codi, usu_userna, usu_passwo, usu_nombre, usu_observ, usu_perfil) VALUES(1, 'admin', 'admin', 'Admin', '', 'sacsystem');
