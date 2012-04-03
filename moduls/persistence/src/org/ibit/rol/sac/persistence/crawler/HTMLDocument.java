9e96b65f086772b653559004f40b8e758706b11e
ejaen-dgtic <ejaen-dgtic@58ef2255-6f3f-491f-b93b-e266552ec6fa>
2012-04-03 09:27:17 +0000

    tid: 0000199 mid: refactorització del codi
    git-svn-id: https://svn.code.sf.net/p/rolsac/code/rolsac-core/branches/rolsac-1.1@487 58ef2255-6f3f-491f-b93b-e266552ec6fa

--- moduls/persistence/src/org/ibit/rol/sac/persistence/crawler/HTMLDocument.java
+++ moduls/persistence/src/org/ibit/rol/sac/persistence/crawler/HTMLDocument.java
@@ -17,14 +17,33 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.lucene.document.*;
 
+
+/*
+ * @author INDRA
+ * 
+ * @author enric - pongo comentarios.
+ * Esta clase se conecta a una pagina html,
+ * lee su contenido, y lo guarda en un Nodo (del
+ * Crawler) para su posterior indexación. 
+ * Si se trata de un Microsite lee unos tags 
+ * determinados, o todo el contenido si no hay tags.
+ * Si se trata de una pagina externa, lee
+ * todo el contenido.
+ * 
+ */
 public class HTMLDocument {
 	protected  Log log = LogFactory.getLog(HTMLDocument.class);
 
+	// TODO es proposa cambiar el nom d'aquesta funció por algo asi com: prepararIndexacionPaginaHtml()
 	public Nodo Document(String idFicha, String url, List<Nodo> arbol,
 			int counter,boolean isMicrosite) throws IOException, InterruptedException {
+		
+		// Para parsear la pagina html ...
+		
 		Document doc = null;
 		Nodo hijo = null;
-		/** Configuracion conexion**/
+		
+		// .. se establece conexion con la pagina
 		StringBean sb = new StringBean();
 		sb.setLinks(false);
 		URL urlTem = new URL(url);
@@ -35,7 +54,7 @@ public class HTMLDocument {
 		uRLConnection.setConnectTimeout(timeOut);
 		sb.setConnection(uRLConnection);
 
-		// Comprobamos que el contenido de la pagina no exista
+		// .. se lee el texto de la pagina y se comprueba en el arbol que el contenido no exista
 		boolean repetida = false;
 	    String md5="";
 		try {
@@ -50,10 +69,12 @@ public class HTMLDocument {
 			}
 		}
 		if (!repetida) {
-			// Solo se indexa en el caso que sea una URL con contenido html. De
-			// esta manera excluimos imagenes, pdfs , zips etc
-
+	
 			try {
+				
+				// .. se indexa la pagina, solo si tiene contenido html. De
+				// esta manera excluimos imagenes, pdfs , zips etc
+
 				if (sb.getConnection() != null) {
 					if (sb.getConnection().getContentType().startsWith(
 							"text/html")) {
@@ -61,6 +82,8 @@ public class HTMLDocument {
 
 						String title = new String();
 
+						// .. se indexa la  url y el instante del tiempo
+						
 						doc.add(new Field("url", url, Field.Store.YES,
 								Field.Index.UN_TOKENIZED));
 
@@ -72,9 +95,12 @@ public class HTMLDocument {
 						Parser bParser=new Parser();
 						bParser.setResource(url);
 						
-						//Capturamos el contenido a indexar. Si hay defidos tags de búsqueda lo sacamos de allí sino indexamos todo
-						
 						try {
+							
+							// .. se comprueba si es un microsite, y en este caso se indexan solo los tags especificados, o todo si no hay tags o
+							// los tags no tienen contenido.
+							
+							
 							String[] tags = getTagsIndexacionMS(idFicha);
 							if(isMicrosite && tags.length>0 ){
 								StringBuffer tempContenido = new StringBuffer();
@@ -93,12 +119,14 @@ public class HTMLDocument {
 									contenido = new StringReader(tempContenido.toString());	
 								}
 								else{
+									// .. si tags no tienen contenido, se indexa todo el texto
 									log.debug("[IndexInsertarFicha:" + idFicha + "] No se ha encontrado contenido en tags, indexamos todo el contenido de la URL");
 									contenido = new StringReader(sb.getStrings());
 								}
 								
 							}
 							else{
+								//.. si no es MS, se indexa todo el texto
 								log.debug("[IndexInsertarFicha:" + idFicha + "] Indexamos todo el contenido de la URL");
 								contenido = new StringReader(sb.getStrings());
 							}
@@ -108,7 +136,7 @@ public class HTMLDocument {
 
 						}
 						
-						//Campturamos el título
+						//.. se indexa el título
 
 						NodeFilter bFilter;
 
@@ -127,9 +155,12 @@ public class HTMLDocument {
 						doc.add(new Field("title", title, Field.Store.YES,
 								Field.Index.TOKENIZED));
 
+						//.. se indexa el id de la ficha
+						
 						doc.add(new Field("idFicha", idFicha, Field.Store.YES,
 								Field.Index.UN_TOKENIZED));
-						// Creamos el hijo y le metemos los datos
+						
+						//.. ponemos los datos a indexar en un nodo
 						hijo = new Nodo(url, counter, doc,md5);
 					}
 
@@ -144,6 +175,9 @@ public class HTMLDocument {
 
 			}
 		}
+		
+		// .. se devuelve el hijo
+		
 		return hijo;
 	}
 	 
