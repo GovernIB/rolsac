1ac11d14c4f9a3ee4090253ba489454cb5f8c3e6
ejaen-dgtic <ejaen-dgtic@58ef2255-6f3f-491f-b93b-e266552ec6fa>
2012-06-12 11:36:08 +0000

    mid: 0000353 msg: integrar els tests unitaris en el target main
    git-svn-id: https://svn.code.sf.net/p/rolsac/code/rolsac-core/branches/rolsac-1.1@552 58ef2255-6f3f-491f-b93b-e266552ec6fa

--- test/persistence/unitario/java/test/unitario/persistence/mock/MockNormativaDelegate.java
+++ test/persistence/unitario/java/test/unitario/persistence/mock/MockNormativaDelegate.java
@@ -1,8 +1,14 @@
 package test.unitario.persistence.mock;
 
+import org.ibit.rol.sac.persistence.delegate.DelegateException;
 import org.ibit.rol.sac.persistence.delegate.NormativaDelegate;
 
 public class MockNormativaDelegate  extends NormativaDelegate {
+
+	protected MockNormativaDelegate() throws DelegateException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	
 
