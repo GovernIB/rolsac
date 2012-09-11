674a506f8b9376f74d59f353fb980fc521e6d0a1
ejaen-dgtic <ejaen-dgtic@58ef2255-6f3f-491f-b93b-e266552ec6fa>
2012-09-11 07:09:29 +0000

    mid: 0000397 msg: integració amb 1.0.4
    git-svn-id: https://svn.code.sf.net/p/rolsac/code/rolsac-core/branches/rolsac-1.1@639 58ef2255-6f3f-491f-b93b-e266552ec6fa

--- test/model/unitario/java/org/ibit/rol/sac/model/DocumentoTest.java
+++ test/model/unitario/java/org/ibit/rol/sac/model/DocumentoTest.java
@@ -6,50 +6,31 @@ import static org.junit.Assert.*;
 public class DocumentoTest {
 
 	@Test
-	public void compruebaStringDocumentoSinArchivo() {
+	public void imprimeStringDocumentoSinTraduccionNiActuacion()  {
 		Documento doc = new Documento();
 		String str = doc.toString();
-		assertFalse(str.contains("archivo=..."));
-		assertFalse(str.contains("Archivo"));
-		assertTrue(str.contains("archivo=null"));
+		assertTrue(!str.contains("ficha"));
+		assertTrue(!str.contains("procedimiento"));
 	}
+
+	@Test
+	public void imprimeStringDocumentoSinTraduccionConFicha()  {
+		Documento doc = new Documento();
+		doc.setFicha(new Ficha(123L));
+		String str = doc.toString();
+		assertTrue(str.contains("ficha"));
+		assertTrue(!str.contains("procedimiento"));
+	}
+
 	
 	@Test
-	public void compruebaStringDocumentoConArchivoEnCatala() {
+	public void imprimeStringDocumentoSinTraduccionConProcedimiento()  {
 		Documento doc = new Documento();
-		TraduccionDocumento tradoc =new TraduccionDocumento();
-		Archivo archivo = new Archivo();
-		String nom="archivo de prueba.txt";
-		Long peso=2222L; 
-		archivo.setNombre(nom);
-		archivo.setPeso(peso);
-		tradoc.setArchivo(archivo);
-		doc.setTraduccion("ca", tradoc);
-		
+		doc.setProcedimiento(new ProcedimientoLocal(123L));
 		String str = doc.toString();
-		assertFalse(str.contains("archivo=..."));
-		assertFalse(str.contains("archivo=null"));
-		assertTrue(str.contains(nom));
-		assertTrue(str.contains(peso.toString()));
-		
+		assertTrue(str.contains("procedimiento"));
+		assertTrue(!str.contains("ficha"));
 	}
-		
+
 	
-	@Test
-	public void compruebaStringDocumentoConArchivoEnCastellaSolamente() {
-		Documento doc = new Documento();
-		TraduccionDocumento tradoc =new TraduccionDocumento();
-		Archivo archivo = new Archivo();
-		String nom="archivo de prueba.txt";
-		Long peso=2222L; 
-		archivo.setNombre(nom);
-		archivo.setPeso(peso);
-		tradoc.setArchivo(archivo);
-		doc.setTraduccion("es", tradoc);
-		
-		String str = doc.toString();
-		assertFalse(str.contains("archivo=..."));
-		assertTrue(str.contains("archivo=null"));
-		
-	}
 }
