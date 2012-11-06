c27a313bc721fff1ab56d995468c5e7771303a5b
jmora-bruat4 <jmora-bruat4@58ef2255-6f3f-491f-b93b-e266552ec6fa>
2012-11-06 12:21:50 +0000

    mid: 000429 msg: No filtrar per UA en la cerca de normatives dins la fitxa de procediments.
    git-svn-id: https://svn.code.sf.net/p/rolsac/code/rolsac-core/branches/rolsac-1.2@713 58ef2255-6f3f-491f-b93b-e266552ec6fa

--- moduls/back2/src/es/caib/rolsac/back2/controller/CatalegProcedimentsBackController.java
+++ moduls/back2/src/es/caib/rolsac/back2/controller/CatalegProcedimentsBackController.java
@@ -1158,7 +1158,9 @@ public class CatalegProcedimentsBackController extends PantallaBaseController {
 			
 			//Realizar la consulta y obtener resultados
 			NormativaDelegate normativaDelegate = DelegateUtil.getNormativaDelegate();
-			llistaNormatives = normativaDelegate.buscarNormativas(paramMap, paramTrad, "local", ua.getId(), false, false, campoOrdenacion, orden);
+			
+			//La búsqueda de normativas no tendrá en cuenta la UA actual (idua = null)
+			llistaNormatives = normativaDelegate.buscarNormativas(paramMap, paramTrad, "local", null, false, false, campoOrdenacion, orden);						
 			
 			for (Normativa normativa : llistaNormatives) {
 				TraduccionNormativa traNor = (TraduccionNormativa)normativa.getTraduccion(idioma);
