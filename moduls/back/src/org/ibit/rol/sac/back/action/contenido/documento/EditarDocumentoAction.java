521075b84f2edc8e90a73ca37faa613cfba4fa23
amartin-bruat4 <amartin-bruat4@58ef2255-6f3f-491f-b93b-e266552ec6fa>
2014-01-30 13:16:47 +0000

    mid: 0000514
    msg: se corrije el borrado/modificación de documentos asociados a fichas en back antiguo.
    
    git-svn-id: https://svn.code.sf.net/p/rolsac/code/rolsac-core/branches/rolsac-1.2@1039 58ef2255-6f3f-491f-b93b-e266552ec6fa

--- moduls/back/src/org/ibit/rol/sac/back/action/contenido/documento/EditarDocumentoAction.java
+++ moduls/back/src/org/ibit/rol/sac/back/action/contenido/documento/EditarDocumentoAction.java
@@ -13,7 +13,6 @@ import org.ibit.rol.sac.model.Documento;
 import org.ibit.rol.sac.model.HechoVital;
 import org.ibit.rol.sac.model.TraduccionDocumento;
 import org.ibit.rol.sac.model.TraduccionHechoVital;
-import org.ibit.rol.sac.persistence.delegate.*;
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -21,6 +20,9 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.upload.FormFile;
+import org.ibit.rol.sac.persistence.delegate.ArchivoDelegate;
+import org.ibit.rol.sac.persistence.delegate.DelegateUtil;
+import org.ibit.rol.sac.persistence.delegate.DocumentoDelegate;
 import org.ibit.rol.sac.persistence.delegate.IdiomaDelegate;
 
 import javax.servlet.http.HttpServletRequest;
@@ -59,113 +61,121 @@ import java.util.*;
 
  *
  */
-public class EditarDocumentoAction extends BaseDispatchAction{
+public class EditarDocumentoAction extends BaseDispatchAction {
 
-    protected static Log log = LogFactory.getLog(EditarDocumentoAction.class);
+	protected static Log log = LogFactory.getLog(EditarDocumentoAction.class);
 
-    protected Map getKeyMethodMap(){
-        Map map = new HashMap();
-        map.put("boton.insertar","editar");
-        map.put("boton.modificar","editar");
-        map.put("boton.seleccionar","seleccionar");
-        map.put("boton.eliminar","eliminar");
-        map.put("boton.cancelar","cancelar");
-        return map;
-    }
+	protected Map getKeyMethodMap(){
+		Map map = new HashMap();
+		map.put("boton.insertar","editar");
+		map.put("boton.modificar","editar");
+		map.put("boton.seleccionar","seleccionar");
+		map.put("boton.eliminar","eliminar");
+		map.put("boton.cancelar","cancelar");
+		return map;
+	}
 
-    public ActionForward editar(ActionMapping mapping, ActionForm form,
-                                HttpServletRequest request,
-                                HttpServletResponse response) throws Exception{
+	public ActionForward editar(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
 
-        log.debug("entramos en editar");
-        DocumentoForm dForm = (DocumentoForm)form;
-        IdiomaDelegate idiomaDelegate = DelegateUtil.getIdiomaDelegate();        
-        DocumentoDelegate docDelegate = DelegateUtil.getDocumentoDelegate();
+		log.debug("entramos en editar");
+		DocumentoForm dForm = (DocumentoForm)form;
+		IdiomaDelegate idiomaDelegate = DelegateUtil.getIdiomaDelegate();        
+		DocumentoDelegate docDelegate = DelegateUtil.getDocumentoDelegate();
+		ArchivoDelegate archivoDelegate = DelegateUtil.getArchivoDelegate();
 
-        Documento documento;
-        if (dForm.get("id") == null){
-            documento = new Documento();
-        } else {
-            //Recuperamos los datos del documento
-            documento = docDelegate.obtenerDocumento((Long)dForm.get("id"));
-        }
+		
+		boolean antiguo = dForm.get("id") != null;
+		Documento documentoOld = null;
+		Documento documento = new Documento();
+		
+		if (antiguo)
+			documentoOld = docDelegate.obtenerDocumento((Long)dForm.get("id"));
 
-        VOUtils.populate(documento, dForm);
-        Documento documentoOld = null;
-        boolean modificant = dForm.get("id") != null;
-        if (modificant){
-            //Recuperamos los datos del hecho vital
-        	documentoOld = (Documento)docDelegate.obtenerDocumento((Long)dForm.get("id"));
-        }        
-        
-        //tractam els fitxers associats a les traduccions
-        Iterator lang = idiomaDelegate.listarLenguajes().iterator();
-        Iterator fichers =  Arrays.asList((FormFile[]) dForm.get("fichers")).iterator();
-        //iteram per tots els idiomes
-        while (lang.hasNext()){
-            log.debug("Entro en el while de ficheros");
-            FormFile fichero = (FormFile) fichers.next();
-            String idioma = (String) lang.next();
-            TraduccionDocumento traduccion = (TraduccionDocumento) documento.getTraduccion(idioma);
-            // tractam fichero
-            if (archivoValido(fichero)){
-                traduccion.setArchivo((populateArchivo(traduccion.getArchivo(), fichero)));
-                documento.setTraduccion(idioma, traduccion);
-            } else if (request.getParameter("borrarfichero_" + idioma) != null){
-                traduccion.setArchivo(null);
-                documento.setTraduccion(idioma, traduccion);
-            } else if (modificant){
-            	TraduccionDocumento traduccionOld = (TraduccionDocumento) documentoOld.getTraduccion(idioma);
-                if (traduccionOld != null && traduccionOld.getArchivo() != null)
-                    traduccion.setArchivo(traduccionOld.getArchivo());
-            }
-        }
-        
-        /*
-        FormFile archivo = (FormFile) dForm.get("fichero");
-        if (archivoValido(archivo)){
-            //borrarArchivo(documento.getArchivo());
-            documento.setArchivo(populateArchivo(documento.getArchivo(), archivo));
-        } else if (request.getParameter("borrarfichero") != null){
-            documento.setArchivo(null);
-        }
+		VOUtils.populate(documento, dForm); // Se recuperan los valores del formulario
 
-        if (documento.getArchivo() != null){
-            //borrarArchivo(documento.getArchivo());
-            dForm.set("nombreFichero",documento.getArchivo().getNombre());
-        } else {
-            dForm.set("nombreFichero", null);
-        }
-*/
-        if(dForm.get("id") != null){
-            request.setAttribute("alert", "confirmacion.modificacion");
-        } else {
-            request.setAttribute("alert", "confirmacion.alta");
-        }
+		//tractam els fitxers associats a les traduccions
+		Iterator lang = idiomaDelegate.listarLenguajes().iterator();
+		Iterator fichers =  Arrays.asList((FormFile[]) dForm.get("fichers")).iterator();
+		Vector<Long> ficherosAborrar = new Vector<Long>();
 
-        if (request.getParameter("idProcedimiento") != null){
-            Long id = new Long(request.getParameter("idProcedimiento"));
-            docDelegate.grabarDocumento(documento, id, null);
-            dForm.set("id",documento.getId());
-            log.debug("Creat/Actualitzat " + documento.getId());
-            request.setAttribute("idSelect", id);
-            request.setAttribute("action", this.getResources(request).getMessage("boton.seleccionar"));
+		//iteram per tots els idiomes
+		while (lang.hasNext()){
+			
+			FormFile fichero = (FormFile) fichers.next();
+			String idioma = (String) lang.next();
+			TraduccionDocumento traDoc = (TraduccionDocumento) documento.getTraduccion(idioma);
+			
+			if (archivoValido(fichero)) {
+				
+				if (antiguo) {
+				
+					TraduccionDocumento traDocOld = (TraduccionDocumento) documentoOld.getTraduccion(idioma);
+					
+					if (request.getParameter("borrarfichero_" + idioma) != null || traDocOld.getArchivo() != null)
+						ficherosAborrar.add(traDocOld.getArchivo().getId());
+					
+				}
+				
+				traDoc.setArchivo((populateArchivo(traDoc.getArchivo(), fichero)));
+				
+			} else if (request.getParameter("borrarfichero_" + idioma) != null && !"".equals(request.getParameter("borrarfichero_" + idioma))) {
+				
+				// Indicamos a la traducci�n del documento que no va a tener asignado el archivo.
+				TraduccionDocumento traDocOld = (TraduccionDocumento) documentoOld.getTraduccion(idioma);
+				ficherosAborrar.add( traDocOld.getArchivo().getId() );
+				traDoc.setArchivo(null);
+				
+			} else if (documentoOld != null) {
 
-            return mapping.findForward("procedimiento");
-        }
-        
-        if (request.getParameter("idFicha") != null){
-            Long id = new Long(request.getParameter("idFicha"));
-            docDelegate.grabarDocumento(documento, null, id);
-            dForm.set("id",documento.getId());
-            log.debug("Creat/Actualitzat " + documento.getId());
-            request.setAttribute("idSelect", id);
-            request.setAttribute("action", this.getResources(request).getMessage("boton.seleccionar"));
+				TraduccionDocumento traDocOld = (TraduccionDocumento) documentoOld.getTraduccion(idioma);
+				
+				if ( traDocOld != null )
+					traDoc.setArchivo(traDocOld.getArchivo());
+				
+			}
+			
+			documento.setTraduccion(idioma, traDoc);
+			
+		}
 
-            return mapping.findForward("ficha");
-        }
-        
-/*
+
+		if (dForm.get("id") != null) {
+			request.setAttribute("alert", "confirmacion.modificacion");
+		} else {
+			request.setAttribute("alert", "confirmacion.alta");
+		}
+
+		if (request.getParameter("idProcedimiento") != null) {
+			Long id = new Long(request.getParameter("idProcedimiento"));
+			docDelegate.grabarDocumento(documento, id, null);
+			dForm.set("id",documento.getId());
+			log.debug("Creat/Actualitzat " + documento.getId());
+			request.setAttribute("idSelect", id);
+			request.setAttribute("action", this.getResources(request).getMessage("boton.seleccionar"));
+
+			return mapping.findForward("procedimiento");
+		}
+
+		if (request.getParameter("idFicha") != null) {
+			Long id = new Long(request.getParameter("idFicha"));
+			docDelegate.grabarDocumento(documento, null, id);
+
+			for (Long idArchivo : ficherosAborrar)
+				archivoDelegate.borrarArchivo(idArchivo);
+
+
+			dForm.set("id",documento.getId());
+			log.debug("Creat/Actualitzat " + documento.getId());
+			request.setAttribute("idSelect", id);
+			request.setAttribute("action", this.getResources(request).getMessage("boton.seleccionar"));
+
+			return mapping.findForward("ficha");
+			
+		}
+
+		/*
         if (request.getParameter("idDocInfTramite") != null){   // param definido en documento/formulario.jsp#form 
             Long id = new Long(request.getParameter("idDocInfTramite"));
             docDelegate.grabarDocumento(documento, null, null, id, null);
@@ -187,106 +197,114 @@ public class EditarDocumentoAction extends BaseDispatchAction{
 
             return mapping.findForward("tramite");
         }
-*/
+		 */
 
-     
+		return mapping.findForward("fail");
+		
+	}
 
-        return mapping.findForward("fail");
-    }
+	public ActionForward seleccionar(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
 
-    public ActionForward seleccionar(ActionMapping mapping, ActionForm form, HttpServletRequest request,
-                                  HttpServletResponse response) throws Exception {
+		log.debug("Entramos en seleccionar");
+		TraDynaValidatorForm dForm = (TraDynaValidatorForm) form;
+		DocumentoDelegate docDelegate = DelegateUtil.getDocumentoDelegate();
 
-        log.debug("Entramos en seleccionar");
-        TraDynaValidatorForm dForm = (TraDynaValidatorForm) form;
-        DocumentoDelegate docDelegate = DelegateUtil.getDocumentoDelegate();
-
-        if (request.getParameter("idSelect") != null){
-            Long id = new Long(request.getParameter("idSelect"));
-            Documento documento = docDelegate.obtenerDocumento(id);
-            VOUtils.describe(dForm, documento);
-           /* if (documento.getArchivo() != null){
+		if (request.getParameter("idSelect") != null){
+			Long id = new Long(request.getParameter("idSelect"));
+			Documento documento = docDelegate.obtenerDocumento(id);
+			VOUtils.describe(dForm, documento);
+			/* if (documento.getArchivo() != null){
                 dForm.set("nombreFichero",documento.getArchivo().getNombre());
             }
-            */
-        } else {
-            return mapping.findForward("fail");
-        }
+			 */
+		} else {
+			return mapping.findForward("fail");
+		}
 
-        return mapping.findForward("success");
-    }
+		return mapping.findForward("success");
+		
+	}
 
-    public ActionForward eliminar(ActionMapping mapping, ActionForm form, HttpServletRequest request,
-                                 HttpServletResponse response) throws Exception {
+	public ActionForward eliminar(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
 
-        log.debug("Entramos en eliminar");
-        DocumentoDelegate docDelegate = DelegateUtil.getDocumentoDelegate();
+		log.debug("Entramos en eliminar");
+		DocumentoDelegate docDelegate = DelegateUtil.getDocumentoDelegate();
 
-        if (request.getParameter("idSelect") != null){
-            Long id = new Long(request.getParameter("idSelect"));
-            log.debug("Eliminado Documento: " + id);
-            docDelegate.borrarDocumento(id);
-            TraDynaValidatorForm dForm = (TraDynaValidatorForm) form;
-            dForm.reset(mapping, request);
+		if (request.getParameter("idSelect") != null) {
+			
+			Long id = new Long(request.getParameter("idSelect"));
+			log.debug("Eliminado Documento: " + id);
+			docDelegate.borrarDocumento(id);
+			TraDynaValidatorForm dForm = (TraDynaValidatorForm) form;
+			dForm.reset(mapping, request);
 
-            
-            String fwd=null;
-            id=null;
-            if (request.getParameter("idProcedimiento") != null) {
-            	id= new Long(request.getParameter("idProcedimiento"));
-            	fwd="procedimiento";
-            }
-            else 
-            if (request.getParameter("idTramite") != null) {
-                id= new Long(request.getParameter("idTramite"));
-                fwd="tramite";
-            }
-            else
-            if (request.getParameter("idFicha") != null) {
-                id= new Long(request.getParameter("idFicha"));
-                fwd="ficha";
-            }
-            
-            request.setAttribute("idSelect", id);
-            request.setAttribute("action", this.getResources(request).getMessage("boton.seleccionar"));
-            return mapping.findForward(fwd);
-        }
+			String fwd = null;
+			id = null;
+			
+			if (request.getParameter("idProcedimiento") != null) {
+				id = new Long(request.getParameter("idProcedimiento"));
+				fwd = "procedimiento";
+			}
+			else 
+				if (request.getParameter("idTramite") != null) {
+					id = new Long(request.getParameter("idTramite"));
+					fwd = "tramite";
+				}
+				else
+					if (request.getParameter("idFicha") != null) {
+						id = new Long(request.getParameter("idFicha"));
+						fwd = "ficha";
+					}
 
-        return mapping.findForward("fail");
-    }
+			request.setAttribute("idSelect", id);
+			request.setAttribute("action", this.getResources(request).getMessage("boton.seleccionar"));
+			
+			return mapping.findForward(fwd);
+		
+		}
 
-    public ActionForward cancelar(ActionMapping mapping, ActionForm form,
-                                   HttpServletRequest request,
-                                   HttpServletResponse response) {
+		return mapping.findForward("fail");
+	
+	}
 
-        log.debug("Entramos en cancelled");
-        TraDynaValidatorForm dForm = (TraDynaValidatorForm)form;
-        dForm.reset(mapping, request);
+	public ActionForward cancelar(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request,
+			HttpServletResponse response) {
 
-        if (request.getParameter("idProcedimiento") != null){
-            Long id = new Long(request.getParameter("idProcedimiento"));
-            request.setAttribute("idSelect", id);
-            request.setAttribute("action", this.getResources(request).getMessage("boton.seleccionar"));
+		log.debug("Entramos en cancelled");
+		TraDynaValidatorForm dForm = (TraDynaValidatorForm)form;
+		dForm.reset(mapping, request);
 
-            return mapping.findForward("procedimiento");
-        }
-        if (request.getParameter("idFicha") != null){
-            Long id = new Long(request.getParameter("idFicha"));
-            request.setAttribute("idSelect", id);
-            request.setAttribute("action", this.getResources(request).getMessage("boton.seleccionar"));
+		if (request.getParameter("idProcedimiento") != null) {
+			Long id = new Long(request.getParameter("idProcedimiento"));
+			request.setAttribute("idSelect", id);
+			request.setAttribute("action", this.getResources(request).getMessage("boton.seleccionar"));
 
-            return mapping.findForward("ficha");
-        }
+			return mapping.findForward("procedimiento");
+		}
+		
+		if (request.getParameter("idFicha") != null) {
+			Long id = new Long(request.getParameter("idFicha"));
+			request.setAttribute("idSelect", id);
+			request.setAttribute("action", this.getResources(request).getMessage("boton.seleccionar"));
 
-        return mapping.findForward("fail");
-    }
+			return mapping.findForward("ficha");
+		}
 
-    protected ActionForward unspecified(ActionMapping mapping, ActionForm form,
-                                        HttpServletRequest request,
-                                        HttpServletResponse response) throws Exception {
+		return mapping.findForward("fail");
+		
+	}
 
-        log.debug("Entramos en unspecified");
-        return null;
-    }
+	protected ActionForward unspecified(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+
+		log.debug("Entramos en unspecified");
+		
+		return null;
+		
+	}
 
 }
