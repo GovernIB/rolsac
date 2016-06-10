a47fd12599113dbf8565f702eaf4ecb62e2f9828
slromero-indra <slromero@indra.es>
2016-06-10 08:40:20 +0200

    FIX #349 Cambio formulario procedimiento

--- moduls/back2/src/es/caib/rolsac/back2/controller/TramiteBackController.java
+++ moduls/back2/src/es/caib/rolsac/back2/controller/TramiteBackController.java
@@ -303,11 +303,9 @@ public class TramiteBackController {
 			    
 			}
 			
-<<<<<<< HEAD
+
 			//#4 si el tramite tiene momento=ini, el procedimiento es público debe tener modelo solicitud obligatoriamente
-=======
-			//#4 si el tramite tiene momento=ini, el procedimiento es pï¿½blico debe tener modelo solicitud obligatoriamente
->>>>>>> branch 'rolsac-1.3' of https://github.com/GovernIB/rolsac.git
+
 			if (edicion && isProcedimientoConEstadoPublicacionPublica && fase == 1 &&
 					(request.getParameter("formularisTramit") == null || request.getParameter("formularisTramit").equals(""))) {
 				
