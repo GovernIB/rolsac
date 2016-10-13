bec5c43d9f1935bf02858aeb405bec670682276b
slromero-indra <slromero@indra.es>
2016-10-13 13:06:32 +0200

    FIX #346 Actualización scripts

--- scripts/bbdd/1.4/oracle/rolsac_create_schema.sql
+++ scripts/bbdd/1.4/oracle/rolsac_create_schema.sql
@@ -258,6 +258,7 @@ drop table RCS_PERGES cascade constraints;
 drop table RCS_TRAPEG cascade constraints;
 drop table RCS_PEGSEC cascade constraints;
 drop table RCS_USUPEG cascade constraints;
+drop table RSC_SOLRPD cascade constraints;
 
 drop sequence RSC_SEQ_COM;
 drop sequence RSC_SEQ_ALL;
@@ -1154,6 +1155,72 @@ CREATE TABLE RSC_USUPEG
   USP_CODUSU  NUMBER(19) NOT NULL,
   USP_CODPEG  NUMBER(19) NOT NULL
 );
+
+-- TAULA RSC_SOLRPD
+CREATE TABLE RSC_SOLRPD
+  (
+    SLP_ID   NUMBER NOT NULL ENABLE,
+    SLP_TIPO VARCHAR2(128 CHAR),
+    SLP_IDELEM NUMBER,
+    SLP_ACCION NUMBER,
+    SLP_FECCRE DATE,
+    SLP_FECIDX DATE,
+    SLP_RESULT NUMBER,
+    SLP_TXTERR VARCHAR2(4000),  
+    CONSTRAINT RSC_SOLRPD_PK PRIMARY KEY (SLP_ID)     
+  );
+  
+  COMMENT ON TABLE  RSC_SOLRPD IS 'Indexacion';
+  COMMENT ON COLUMN RSC_SOLRPD.SLP_ID       IS 'Identificador';
+  COMMENT ON COLUMN RSC_SOLRPD.SLP_TIPO     IS 'Tipo';
+  COMMENT ON COLUMN RSC_SOLRPD.SLP_IDELEM   IS 'Identificador Elemento';
+  COMMENT ON COLUMN RSC_SOLRPD.SLP_ACCION   IS 'Accion. 1 - Indexar, 2 - Borrar';
+  COMMENT ON COLUMN RSC_SOLRPD.SLP_FECCRE   IS 'Fecha Creacion';
+  COMMENT ON COLUMN RSC_SOLRPD.SLP_FECIDX   IS 'Fecha Ejecucion de indexacion';
+  COMMENT ON COLUMN RSC_SOLRPD.SLP_RESULT   IS 'Resultado.  0 - Pendiente, 1 Ejecutado correctamente, -1 Ejecutado con errores';
+  COMMENT ON COLUMN RSC_SOLRPD.SLP_TXTERR   IS 'Mensaje de error que indique porque no se ha indexado';
+  
+  
+---tabla solr job 
+CREATE TABLE RSC_SOLJOB
+  (
+    JOB_ID NUMBER(19,0) NOT NULL ENABLE,
+    JOB_FECINI DATE DEFAULT SYSDATE,
+    JOB_FECFIN DATE,
+    JOB_FECFCH DATE,
+    JOB_FECPRO DATE,
+    JOB_FECNOR DATE,
+    JOB_FECTRA DATE,
+    JOB_FECUNA DATE,
+    JOB_TOTFCH NUMBER(5,2) DEFAULT 0,
+    JOB_TOTDFC NUMBER(5,2) DEFAULT 0,
+    JOB_TOTPRO NUMBER(5,2) DEFAULT 0,
+    JOB_TOTDPR NUMBER(5,2) DEFAULT 0,
+    JOB_TOTNOR NUMBER(5,2) DEFAULT 0,
+    JOB_TOTDNO NUMBER(5,2) DEFAULT 0,
+    JOB_TOTTRA NUMBER(5,2) DEFAULT 0,
+    JOB_TOTUNA NUMBER(5,2) DEFAULT 0,
+    CONSTRAINT RSC_JOB_PK PRIMARY KEY (JOB_ID) 
+  );
+
+ -- Comentarios de la tabla solr job
+  COMMENT ON COLUMN RSC_SOLJOB.JOB_ID IS 'Identificador de la clase.';
+  COMMENT ON COLUMN RSC_SOLJOB.JOB_FECINI IS 'Fecha de inicio';
+  COMMENT ON COLUMN RSC_SOLJOB.JOB_FECFIN IS 'Fecha fin';
+  COMMENT ON COLUMN RSC_SOLJOB.JOB_FECFCH IS 'Fecha fin ejecutando fichas.';
+  COMMENT ON COLUMN RSC_SOLJOB.JOB_FECPRO IS 'Fecha fin ejecutando procedimientos.';
+  COMMENT ON COLUMN RSC_SOLJOB.JOB_FECNOR IS 'Fecha fin ejecutando normativas.';
+  COMMENT ON COLUMN RSC_SOLJOB.JOB_FECTRA IS 'Fecha fin ejecutando trámites.';
+  COMMENT ON COLUMN RSC_SOLJOB.JOB_FECUNA IS 'Fecha fin ejecutando unidad administrativa';
+  COMMENT ON COLUMN RSC_SOLJOB.JOB_TOTFCH IS 'Porcentaje de total fichas';
+  COMMENT ON COLUMN RSC_SOLJOB.JOB_TOTDFC IS 'Porcentaje de total fichas documento';
+  COMMENT ON COLUMN RSC_SOLJOB.JOB_TOTPRO IS 'Porcentaje de total procedimiento';
+  COMMENT ON COLUMN RSC_SOLJOB.JOB_TOTDPR IS 'Porcentaje de total procedimiento documento';
+  COMMENT ON COLUMN RSC_SOLJOB.JOB_TOTNOR IS 'Porcentaje de total normativas';
+  COMMENT ON COLUMN RSC_SOLJOB.JOB_TOTDNO IS 'Porcentaje de total normativas documento';
+  COMMENT ON COLUMN RSC_SOLJOB.JOB_TOTTRA IS 'Porcentaje de total tramite';
+  COMMENT ON COLUMN RSC_SOLJOB.JOB_TOTUNA IS 'Porcentaje de total Unid. Adminitr.';
+  
 
 alter table RSC_PROMAT add constraint RSC_PRMMAT_FK foreign key (PRM_CODMAT) references RSC_MATERI;
 alter table RSC_PROMAT add constraint RSC_PRMPRO_FK foreign key (PRM_CODPRO) references RSC_PROCED;
