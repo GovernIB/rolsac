c3bfe75ebe4c623e1763d765a8d17c2663d65236
slromero-indra <slromero@indra.es>
2016-12-22 09:43:04 +0100

    #346 Errores de indexación
    Se ha arreglado un problema en la comprobación de la fase de un trámite
    para que compruebe antes que el trámite no sea nulo.

--- moduls/persistence/src/org/ibit/rol/sac/persistence/ejb/ProcedimientoFacadeEJB.java
+++ moduls/persistence/src/org/ibit/rol/sac/persistence/ejb/ProcedimientoFacadeEJB.java
@@ -2241,7 +2241,7 @@ public abstract class ProcedimientoFacadeEJB extends HibernateEJB implements Pro
 					indexData.setTelematico(true);					
 				}
 				
-				if (tramite.getFase() == Tramite.INICIACION) {
+				if (tramite != null && tramite.getFase() == Tramite.INICIACION) {
 					indexData.setFechaPlazoIni(tramite.getDataInici());
 					indexData.setFechaPlazoFin(tramite.getDataTancament());
 				}
