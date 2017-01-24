a2101e72c8f1a637b7b5c28689036fdc599e4287
slromero-indra <slromero@indra.es>
2017-01-24 14:09:23 +0100

    #377 Inclusión de un miniscript para actualizar el estado sia.
    Actualiza el estado SIA de aquellos procedimientos que tengan código SIA
    pero no tengan estado SIA. Les pone como alta.

--- scripts/bbdd/1.5/oracle/rolsac_update_data_from_1.4.0_to_1.5.0.sql
+++ scripts/bbdd/1.5/oracle/rolsac_update_data_from_1.4.0_to_1.5.0.sql
@@ -2,3 +2,9 @@
        SET (tfi_banner, tfi_imagen, tfi_icono) = (SELECT ficha.fic_baner, ficha.fic_imagen, ficha.fic_icono
                                                     FROM RSC_FICHA ficha
                                                    WHERE traduccionficha.tfi_codfic = ficha.fic_codi);
+                                                   
+       /** Actualiza el estado de SIA a ALTA de aquellos que ya tengan código SIA y estén si estado. **/                                            
+update rsc_proced
+     set pro_estsia = 'A'
+  where pro_codsia is not null
+    and pro_estsia is null;                                                   
